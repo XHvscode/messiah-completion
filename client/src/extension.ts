85e28f11f6dfceb70238cb5d3e4c96f22009e781
xiaohao <xiaohao@corp.netease.com>
2022-07-28 21:54:10 +0800

    client

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -3,6 +3,7 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
+import * as net from "net";
 import * as path from 'path';
 import { workspace, ExtensionContext } from 'vscode';
 
@@ -16,33 +17,28 @@ import {
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-	// The server is implemented in node
-	const serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'server.js')
-	);
-	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
-
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	const serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-			options: debugOptions
-		}
-	};
 
 	// Options to control the language client
 	const clientOptions: LanguageClientOptions = {
 		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+		documentSelector: [{ scheme: 'file', language: 'python' }],
+		outputChannelName: "[pygls] PythonLanguageServer",
 		synchronize: {
 			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+			fileEvents: workspace.createFileSystemWatcher('**/g83_jump_config.json')
 		}
+	};
+
+	const serverOptions: ServerOptions = () => {
+		return new Promise((resolve /*, reject */) => {
+			const clientSocket = new net.Socket();
+			clientSocket.connect(16471, "127.0.0.1", () => {
+				resolve({
+					reader: clientSocket,
+					writer: clientSocket,
+				});
+			});
+		});
 	};
 
 	// Create the language client and start the client.
