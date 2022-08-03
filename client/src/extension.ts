b4430a7bf3f004f93a65a8e454558aa9b2317900
xiaohao <xiaohao@corp.netease.com>
2022-08-03 21:47:16 +0800

    优化

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -16,9 +16,10 @@ import {
 
 let client: LanguageClient;
 
+
+
+
 export function activate(context: ExtensionContext) {
-
-
 
     // Options to control the language client
     const clientOptions: LanguageClientOptions = {
@@ -27,36 +28,60 @@ export function activate(context: ExtensionContext) {
         outputChannelName: "[pygls] PythonLanguageServer",
         synchronize: {
             // Notify the server about file changes to '.clientrc files contained in the workspace
-            fileEvents: workspace.createFileSystemWatcher('./.vscode/class_relation.json')
+            fileEvents: workspace.createFileSystemWatcher('**/class_relation.json')
         }
     };
 
-    const serverOptions: ServerOptions = () => {
-        return new Promise((resolve /*, reject */) => {
-            const clientSocket = new net.Socket();
-            clientSocket.connect(16471, "127.0.0.1", () => {
-                resolve({
-                    reader: clientSocket,
-                    writer: clientSocket,
-                });
-            });
-        });
-    };
 
     if (context.extensionMode === ExtensionMode.Development) {
         // Create the language client and start the client.
+
+        const serverOptions: ServerOptions = () => {
+            return new Promise((resolve /*, reject */) => {
+                const clientSocket = new net.Socket();
+                clientSocket.connect(16471, "127.0.0.1", () => {
+                    resolve({
+                        reader: clientSocket,
+                        writer: clientSocket,
+                    });
+                });
+            });
+        };
+
         client = new LanguageClient(
             'languageServerExample',
             'Language Server Example',
             serverOptions,
             clientOptions
         );
+
     } else {
-        client = new LanguageClient("server/main.xh", serverOptions, clientOptions);
+
+        function startLangServer(
+            command: string,
+            args: string[],
+            cwd: string
+        ): LanguageClient {
+            const serverOptions: ServerOptions = {
+                args,
+                command,
+                options: { cwd },
+            };
+
+            return new LanguageClient(command, serverOptions, clientOptions);
+        }
+        const cwd = path.join(__dirname, "..", "..");
+
+        client = startLangServer("server/main.xh", [], cwd);
     }
 
     // Start the client. This will also launch the server
-    client.start();
+    // client.start();
+    const disposable = client.start();
+    context.subscriptions.push(disposable);
+    client.onReady().then(() => {
+        console.log('lsp-messiah server is ready');
+    });
 }
 
 export function deactivate(): Thenable<void> | undefined {
