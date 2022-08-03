18a1665d933ba5c4041bcd04dbe4c6c1ef3aacfc
xiaohao <xiaohao@corp.netease.com>
2022-08-03 16:46:18 +0800

    add build

---
+++ server/build.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+# Date: 2022-08-03 16:08:17
+# Author: xiaohao@corp.netease.com
+"""
+
+"""
+
+import os
+import shutil
+
+GAME_NAME = "main"
+sCurPath = os.getcwd()
+print(sCurPath)
+
+exeDir = os.path.join(sCurPath, "exe")
+if os.path.exists(exeDir):
+    shutil.rmtree(exeDir)
+    print("delete exe")
+
+sCmd = "pyinstaller -F \
+--workpath=%s/exe/build \
+--distpath=%s/exe/dist \
+--specpath=%s/exe \
+--name=%s \
+-pathes=%s;C:/Python38/DLLs;C:/Python38/Lib \
+%s/main.py"\
+% (sCurPath, sCurPath, sCurPath, GAME_NAME, sCurPath, sCurPath)
+
+os.system(sCmd)
